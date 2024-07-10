06d76074c4c04cfb32582919020beb572f0121d2
cn33143 <cn33143@powersi.com>
2024-07-10 23:22:40 +0800

    收入托盘、进程永驻

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,10 @@
     windows_subsystem = "windows"
 )]
 
+
+mod tray;
+use tauri::{WindowEvent};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +15,17 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .system_tray(tray::main_menu())
+        .on_system_tray_event(tray::handler)
         .invoke_handler(tauri::generate_handler![greet])
+        // 让 app 保持在后台运行：https://tauri.app/v1/guides/features/system-tray/#preventing-the-app-from-closing
+        .on_window_event(|event| match event.event() {
+            WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
